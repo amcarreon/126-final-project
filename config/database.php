bdd2845b721c8b96d2812bbce885979e83c577db
amcarreon <atcarreon1@up.edu.ph>
2026-05-03 17:22:29 +0800

    register authentication and validation of inputs

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "byteXpress_sys";
+$dbname = "byteXpress_schema";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
--- includes/utility_functions.php
+++ includes/utility_functions.php
@@ -1,24 +1,50 @@
 <?php
+require_once 'config/constants.php';
 
     function validate_email($email) {
-
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     function validate_password($password) {
-        
+        $hasUppercase = preg_match('/[A-Z]/', $password);
+        $hasNum = preg_match('/[0-9]/', $password);
+        $hasSpecial = preg_match('/[^a-zA-Z\d]/', $password);
+
+        if (!(strlen($password)>PASSWORD_MIN_LENGTH)) {
+            return "Password must be at least " . PASSWORD_MIN_LENGTH . " characters.";
+        }
+
+        if (PASSWORD_REQUIRE_UPPERCASE && !$hasUppercase) {
+            return "Password must have at least 1 uppercase letter.";
+        }
+
+        if (PASSWORD_REQUIRE_NUMBERS && !$hasNum) {
+            return "Password must have at least 1 number letter.";
+        }
+
+        if (PASSWORD_REQUIRE_SPECIAL && !$hasSpecial) {
+            return "Password must have at least 1 special character.";
+        }
+
+        return true;
     }
 
     function sanitize($input, $type = 'default') {
         $input = trim($input);
         $input = stripslashes($input);
+        $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
 
         switch ($type) {
             case 'email':
-                // to be filled
+                return filter_var($input, FILTER_SANITIZE_EMAIL);
             case 'name':
-                // to be filled
             default:
-                return htmlspecialchars(strip_tags($input), ENT_QUOTES, 'UTF-8');
+                return $input;
         }
     }
 
