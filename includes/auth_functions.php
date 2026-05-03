dadec9a08d21eb9dc4d90185133285a5fcc807c1
amcarreon <atcarreon1@up.edu.ph>
2026-05-03 20:35:51 +0800

    updated registration wrt the validated inputs

--- includes/auth_functions.php
+++ includes/auth_functions.php
@@ -1,11 +1,20 @@
 <?php
+require_once '126-final-project/config/database.php';
+require_once 'utility_functions.php';
 
 function register_user($email, $full_name, $password, $user_type, $phone_number) {
     global $conn;
 
+    $valid_email = validate_email($email)
+    $valid_pass = validate_password($password);
+
+    if (!$valid_email && !$valid_pass) {
+        return;
+    }
+
     $hashed_pass = password_hash($password, PASSWORD_DEFAULT);
 
-    $sql = "INSERT INTO users (email, full_name, pass, phone_number, user_type)
+    $sql = "INSERT INTO users (email, full_name, password_hash, phone_number, user_type)
             VALUES (?, ?, ?, ?, ?)"
     $stmt = $conn->prepare();
     $stmt->bind_param("sssss", $email, $full_name, $hashed_pass, $phone_number, $user_type);
--- includes/utility_functions.php
+++ includes/utility_functions.php
@@ -1,34 +1,51 @@
 <?php
-require_once 'config/constants.php';
+require_once '126-final-project/config/constants.php';
 
     function validate_email($email) {
-        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            return true;
-        }
-        else {
+        // Validation of format
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            //return "Invalid email format.";
             return false;
         }
+
+        // Checking for duplicates
+        $findDupe = $conn->prepare("SELECT email FROM Users WHERE email = ?");
+        $findDupe->bind_param("s", $email);
+        $findDupe->execute();
+        $res = $findDupe->get_result();
+
+        if($res->num_rows > 0) {
+            //return "Email already exists.";
+            return false;
+        }
+            
     }
 
     function validate_password($password) {
+        // Regex
         $hasUppercase = preg_match('/[A-Z]/', $password);
         $hasNum = preg_match('/[0-9]/', $password);
         $hasSpecial = preg_match('/[^a-zA-Z\d]/', $password);
 
-        if (!(strlen($password)>PASSWORD_MIN_LENGTH)) {
-            return "Password must be at least " . PASSWORD_MIN_LENGTH . " characters.";
+        // Validations
+        if (strlen($password)<PASSWORD_MIN_LENGTH) {
+            //return "Password must be at least " . PASSWORD_MIN_LENGTH . " characters.";
+            return false;
         }
 
         if (PASSWORD_REQUIRE_UPPERCASE && !$hasUppercase) {
-            return "Password must have at least 1 uppercase letter.";
+            //return "Password must have at least 1 uppercase letter.";
+            return false;
         }
 
         if (PASSWORD_REQUIRE_NUMBERS && !$hasNum) {
-            return "Password must have at least 1 number letter.";
+            //return "Password must have at least 1 number letter.";
+            return false;
         }
 
         if (PASSWORD_REQUIRE_SPECIAL && !$hasSpecial) {
-            return "Password must have at least 1 special character.";
+            //return "Password must have at least 1 special character.";
+            return false;
         }
 
         return true;
