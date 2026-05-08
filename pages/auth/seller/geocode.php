c667cd67fa8811c45773d1f10f05453d1f946ba1
amcarreon <atcarreon1@up.edu.ph>
2026-05-08 23:08:07 +0800

    address validation

---
+++ pages/auth/seller/geocode.php
@@ -0,0 +1,40 @@
+<? php
+
+if ($_SERVER['REQUEST METHOD'] === 'POST' && !empty($_POST['location'])) {
+    $address = $_POST['location'] ?? ;
+
+    $encodedAddress = urlencode($address);
+
+    $url =
+        "https://maps.googleapis.com/maps/api/geocode/json"
+        . "?address={$encodedAddress}"
+        . "&key=API_KEY";
+
+    $res = file_get_contents($url);
+
+    $data = json_decode($response, true);
+
+    if ($data['status'] !== 'OK') {
+        return [
+            'success' => false,
+            'message' => 'Invalid Location'
+        ];
+    }
+
+    $res = $data['results'][0];
+
+    return [
+        'success' => true,
+
+        'formatted_address' =>
+            $res['formatted_address'],
+
+        'latitude' =>
+            $res['geometry']['location']['lat'],
+
+        'longitude' =>
+            $res['geometry']['location']['lng']
+    ];
+}
+
+?>
